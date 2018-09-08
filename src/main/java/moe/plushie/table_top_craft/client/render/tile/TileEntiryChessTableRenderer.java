0b4cac385174a88a081c734904344e34693e0859
RiskyKen <RiskyKen@users.noreply.github.com>
2018-09-08 13:08:42 +0100

    Created grid render.

--- src/main/java/moe/plushie/table_top_craft/client/render/tile/TileEntiryChessTableRenderer.java
+++ src/main/java/moe/plushie/table_top_craft/client/render/tile/TileEntiryChessTableRenderer.java
@@ -3,6 +3,7 @@ package moe.plushie.table_top_craft.client.render.tile;
 import moe.plushie.table_top_craft.client.model.ChessKnight;
 import moe.plushie.table_top_craft.client.model.ChessPawn;
 import moe.plushie.table_top_craft.client.model.ChessRook;
+import moe.plushie.table_top_craft.common.games.chess.ChessPieceType;
 import moe.plushie.table_top_craft.common.lib.ModReference;
 import moe.plushie.table_top_craft.common.tileentities.TileEntityChessTable;
 import net.minecraft.client.renderer.GlStateManager;
@@ -16,9 +17,13 @@ public class TileEntiryChessTableRenderer extends TileEntitySpecialRenderer<Tile
 
     private static final ResourceLocation PAWN_TEXTURE_BLACK = new ResourceLocation(ModReference.MOD_ID, "textures/models/black_pieces.png");
     private static final ResourceLocation PAWN_TEXTURE_WHITE = new ResourceLocation(ModReference.MOD_ID, "textures/models/white_pieces.png");
+    
     private final ChessPawn pawnModel;
     private final ChessRook rookModel;
     private final ChessKnight knightModel;
+    
+    private static final float SCALE_MC = 0.0625F;
+    private static final float SCALE_CHESS = 0.125F;
     
     public TileEntiryChessTableRenderer() {
         pawnModel = new ChessPawn();
@@ -28,48 +33,83 @@ public class TileEntiryChessTableRenderer extends TileEntitySpecialRenderer<Tile
     
     @Override
     public void render(TileEntityChessTable te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
-        float scale = 1F / 16F;
         GlStateManager.pushMatrix();
-        GlStateManager.translate(x + 0.75F, y + 1.02F, z - 0.125F);
+        GlStateManager.pushAttrib();
+        GlStateManager.translate(x + 1F, y + 1.02F, z);
         GlStateManager.scale(1F, -1F, -1F);
-        GlStateManager.rotate(90.0F, 0.0F, 1.0F, 0.0F);
         GlStateManager.enableNormalize();
-        bindTexture(PAWN_TEXTURE_BLACK);
-        
-        GlStateManager.pushMatrix();
-        for (int i = 0; i < 8; i++) {
-            GlStateManager.translate(0.125F, 0F, 0F);
-            pawnModel.render(null, 0, 0, 0, 0, 0, scale);
-        }
+        GlStateManager.translate(-SCALE_CHESS, 0F, -SCALE_CHESS);
+        renderWhitePieces();
+        renderBlackPieces();
+        GlStateManager.popAttrib();
         GlStateManager.popMatrix();
-        
-        GlStateManager.pushMatrix();
-        GlStateManager.translate(0, 0, 0.126F);
-        for (int i = 0; i < 8; i++) {
-            GlStateManager.translate(0.125F, 0F, 0F);
-            rookModel.render(null, 0, 0, 0, 0, 0, scale);
-        }
-        GlStateManager.popMatrix();
-        
-        GlStateManager.translate(1.25F, 0.0F, -0.5F);
-        GlStateManager.rotate(180.0F, 0.0F, 1.0F, 0.0F);
+    }
+    
+    private void renderWhitePieces() {
         bindTexture(PAWN_TEXTURE_WHITE);
-        
-        GlStateManager.pushMatrix();
+        // pawn row
         for (int i = 0; i < 8; i++) {
-            GlStateManager.translate(0.125F, 0F, 0F);
-            pawnModel.render(null, 0, 0, 0, 0, 0, scale);
+            renderChessPiece(ChessPieceType.PAWN, i, 1, false);
         }
-        GlStateManager.popMatrix();
+        renderChessPiece(ChessPieceType.ROOK, 0, 0, false);
+        renderChessPiece(ChessPieceType.ROOK, 7, 0, false);
         
-        GlStateManager.pushMatrix();
-        GlStateManager.translate(0, 0, 0.126F);
+        renderChessPiece(ChessPieceType.KNIGHT, 1, 0, false);
+        renderChessPiece(ChessPieceType.KNIGHT, 6, 0, false);
+        
+        renderChessPiece(ChessPieceType.BISHOP, 2, 0, false);
+        renderChessPiece(ChessPieceType.BISHOP, 5, 0, false);
+        
+        renderChessPiece(ChessPieceType.QUEEN, 3, 0, false);
+        renderChessPiece(ChessPieceType.KING, 4, 0, false);
+    }
+    
+    private void renderBlackPieces() {
+        bindTexture(PAWN_TEXTURE_BLACK);
+        // pawn row
         for (int i = 0; i < 8; i++) {
-            GlStateManager.translate(0.125F, 0F, 0F);
-            knightModel.render(null, 0, 0, 0, 0, 0, scale);
+            renderChessPiece(ChessPieceType.PAWN, i, 6, true);
         }
-        GlStateManager.popMatrix();
-
+        renderChessPiece(ChessPieceType.ROOK, 0, 7, true);
+        renderChessPiece(ChessPieceType.ROOK, 7, 7, true);
+        
+        renderChessPiece(ChessPieceType.KNIGHT, 1, 7, true);
+        renderChessPiece(ChessPieceType.KNIGHT, 6, 7, true);
+        
+        renderChessPiece(ChessPieceType.BISHOP, 2, 7, true);
+        renderChessPiece(ChessPieceType.BISHOP, 5, 7, true);
+        
+        renderChessPiece(ChessPieceType.QUEEN, 3, 7, true);
+        renderChessPiece(ChessPieceType.KING, 4, 7, true);
+    }
+    
+    private void renderChessPiece(ChessPieceType pieceType, int x, int y, boolean flipped) {
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(-SCALE_CHESS * x, 0, -SCALE_CHESS * y);
+        if (flipped) {
+            GlStateManager.rotate(180.0F, 0.0F, 1.0F, 0.0F);
+            GlStateManager.translate(-SCALE_CHESS, 0, -SCALE_CHESS);
+        }
+        switch (pieceType) {
+        case KING:
+            //kingModel.render(null, 0, 0, 0, 0, 0, SCALE_MC);
+            break;
+        case QUEEN:
+            //queenModel.render(null, 0, 0, 0, 0, 0, SCALE_MC);
+            break;
+        case KNIGHT:
+            knightModel.render(null, 0, 0, 0, 0, 0, SCALE_MC);
+            break;
+        case BISHOP:
+            //bishopModel.render(null, 0, 0, 0, 0, 0, SCALE_MC);
+            break;
+        case ROOK:
+            rookModel.render(null, 0, 0, 0, 0, 0, SCALE_MC);
+            break;
+        case PAWN:
+            pawnModel.render(null, 0, 0, 0, 0, 0, SCALE_MC);
+            break;
+        }
         GlStateManager.popMatrix();
     }
 }

e03c4a97b9101e833616da378c04e00dc07278a7
andrew0030 <36803991+andrew0030@users.noreply.github.com>
2018-10-07 16:52:39 +0300

    Changed ChessBlock Model and created costume Boundingbox
    New Model for the Chess with a boundingbox thats smaller then 1 block.

--- src/main/java/moe/plushie/table_top_craft/common/blocks/BlockChessTable.java
+++ src/main/java/moe/plushie/table_top_craft/common/blocks/BlockChessTable.java
@@ -15,10 +15,14 @@ import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockChessTable extends ModBlockContainer implements IHasModel {
+	
+	private static final AxisAlignedBB BOUNDING_BOX = new AxisAlignedBB(0F, 0F, 0F, 1.0F, 0.0625F * 12F, 1.0F);
 
     public static final PropertyDirection STATE_FACING = BlockHorizontal.FACING;
     
@@ -80,5 +84,15 @@ public class BlockChessTable extends ModBlockContainer implements IHasModel {
     @Override
     public TileEntity createNewTileEntity(World worldIn, int meta) {
         return new TileEntityChessTable();
+    }
+    
+    @Override
+    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+    	return BOUNDING_BOX;
+    }
+    
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+    	return BOUNDING_BOX;
     }
 }

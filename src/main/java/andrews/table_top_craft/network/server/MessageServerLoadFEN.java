2a9af5bfdb2d4748ea7091d2c831ad0fcc4cba5c
andrew0030 <36803991+andrew0030@users.noreply.github.com>
2022-12-19 05:46:36 +0200

    Update 1.18.2-1.0.5 Fabric
    - Improved the FEN validator

--- src/main/java/andrews/table_top_craft/network/server/MessageServerLoadFEN.java
+++ src/main/java/andrews/table_top_craft/network/server/MessageServerLoadFEN.java
@@ -37,7 +37,7 @@ public class MessageServerLoadFEN
 					if(blockEntity instanceof ChessTileEntity chessTileEntity)
 			        {
 						Board board = Board.createStandardBoard();
-						if(isFENValid(FEN))
+						if(FenUtil.isFENValid(FEN))
 						{
 							board = FenUtil.createGameFromFEN(FEN);
 						}
@@ -52,54 +52,5 @@ public class MessageServerLoadFEN
 				}
 			});
 		});
-	}
-	
-	private static boolean isFENValid(String FEN)
-	{
-		String[] FENInfo = FEN.split(" ");
-		String[] boardTileInfo = FENInfo[0].split("/");
-		int splitCounter = StringUtils.countMatches(FENInfo[0], "/");
-		int whiteKingCounter = StringUtils.countMatches(FENInfo[0], "K");
-		int blackKingCounter = StringUtils.countMatches(FENInfo[0], "k");
-		
-		if(!(FENInfo.length == 6))
-			return false;
-		if(!(splitCounter == 7))
-			return false;
-		for(int i = 0; i < 8; i++)
-		{
-			String rowInfo = boardTileInfo[i];
-			int valueCounter = 0;
-			
-			for(int j = 0; j < rowInfo.length(); j++)
-			{
-				if(rowInfo.charAt(j) == '1')
-					valueCounter += 1;
-				else if(rowInfo.charAt(j) == '2')
-					valueCounter += 2;
-				else if(rowInfo.charAt(j) == '3')
-					valueCounter += 3;
-				else if(rowInfo.charAt(j) == '4')
-					valueCounter += 4;
-				else if(rowInfo.charAt(j) == '5')
-					valueCounter += 5;
-				else if(rowInfo.charAt(j) == '6')
-					valueCounter += 6;
-				else if(rowInfo.charAt(j) == '7')
-					valueCounter += 7;
-				else if(rowInfo.charAt(j) == '8')
-					valueCounter += 8;
-				else
-					valueCounter += 1;
-			}
-			
-			if(!(valueCounter == 8))
-				return false;
-		}
-		if(!(whiteKingCounter == 1))
-			return false;
-		if(!(blackKingCounter == 1))
-			return false;
-		return FENInfo[1].equals("w") || FENInfo[1].equals("b");
 	}
 }

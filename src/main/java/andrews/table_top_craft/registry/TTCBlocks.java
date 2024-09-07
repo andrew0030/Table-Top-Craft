0bf4143d4e39b41b45f7524cb95ebde21d227bb2
andrew0030 <36803991+andrew0030@users.noreply.github.com>
2024-09-07 05:31:57 +0300

    Update 1.18.2-1.1.0 Fabric
    - Added 32 Chess Board Variants.
    - Fixed incompatibility issue caused by [Fabric API 0.77.0] due to the new core shader registration system.
    - Replaced old chess table model with new more simplistic one, and adjusted taken piece positions.
    - Replaced old tic tac toe textures, and replaced them with more simplistic ones.
    - Adjusted tic tac toe colors, they now utilize the minecraft dye system.
    - Optimized the Default and Classic Chess Pieces.

--- src/main/java/andrews/table_top_craft/registry/TTCBlocks.java
+++ src/main/java/andrews/table_top_craft/registry/TTCBlocks.java
@@ -11,24 +11,58 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.material.Material;
 
 import javax.annotation.Nullable;
 
 public class TTCBlocks
 {
-	// Chess
-	public static final Block OAK_CHESS				= createBlock("oak_chess", new ChessBlock(), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
-	public static final Block SPRUCE_CHESS			= createBlock("spruce_chess", new ChessBlock(), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
-	public static final Block BIRCH_CHESS			= createBlock("birch_chess", new ChessBlock(), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
-	public static final Block JUNGLE_CHESS			= createBlock("jungle_chess", new ChessBlock(), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
-	public static final Block ACACIA_CHESS			= createBlock("acacia_chess", new ChessBlock(), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
-	public static final Block DARK_OAK_CHESS		= createBlock("dark_oak_chess", new ChessBlock(), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
-	public static final Block CRIMSON_CHESS			= createBlock("crimson_chess", new ChessBlock(), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
-	public static final Block WARPED_CHESS			= createBlock("warped_chess", new ChessBlock(), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	// Tic Tac Toe
+	public static final Block TIC_TAC_TOE					= createBlock("tic_tac_toe", new TicTacToeBlock(), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
 	// Chess Piece Figure
-	public static final Block CHESS_PIECE_FIGURE	= createISBERBlock("chess_piece_figure", new ChessPieceFigureBlock(), false, TableTopCraft.TABLE_TOP_CRAFT_GROUP);
-	//Tic Tac Toe
-	public static final Block TIC_TAC_TOE			= createBlock("tic_tac_toe", new TicTacToeBlock(), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block CHESS_PIECE_FIGURE			= createISBERBlock("chess_piece_figure", new ChessPieceFigureBlock(), false, TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	// Chess
+	public static final Block OAK_CHESS						= createBlock("oak_chess", new ChessBlock(Material.WOOD, SoundType.WOOD), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block SPRUCE_CHESS					= createBlock("spruce_chess", new ChessBlock(Material.WOOD, SoundType.WOOD), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block BIRCH_CHESS					= createBlock("birch_chess", new ChessBlock(Material.WOOD, SoundType.WOOD), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block JUNGLE_CHESS					= createBlock("jungle_chess", new ChessBlock(Material.WOOD, SoundType.WOOD), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block ACACIA_CHESS					= createBlock("acacia_chess", new ChessBlock(Material.WOOD, SoundType.WOOD), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block DARK_OAK_CHESS				= createBlock("dark_oak_chess", new ChessBlock(Material.WOOD, SoundType.WOOD), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block CRIMSON_CHESS					= createBlock("crimson_chess", new ChessBlock(Material.WOOD, SoundType.WOOD), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block WARPED_CHESS					= createBlock("warped_chess", new ChessBlock(Material.WOOD, SoundType.WOOD), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block WHITE_TERRACOTTA_CHESS 	  	= createBlock("white_terracotta_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block ORANGE_TERRACOTTA_CHESS 	  	= createBlock("orange_terracotta_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block MAGENTA_TERRACOTTA_CHESS 	  	= createBlock("magenta_terracotta_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block LIGHT_BLUE_TERRACOTTA_CHESS	= createBlock("light_blue_terracotta_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block YELLOW_TERRACOTTA_CHESS 	  	= createBlock("yellow_terracotta_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block LIME_TERRACOTTA_CHESS 	  	= createBlock("lime_terracotta_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block PINK_TERRACOTTA_CHESS 	  	= createBlock("pink_terracotta_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block GRAY_TERRACOTTA_CHESS 	  	= createBlock("gray_terracotta_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block LIGHT_GRAY_TERRACOTTA_CHESS 	= createBlock("light_gray_terracotta_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block CYAN_TERRACOTTA_CHESS 	  	= createBlock("cyan_terracotta_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block PURPLE_TERRACOTTA_CHESS 	  	= createBlock("purple_terracotta_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block BLUE_TERRACOTTA_CHESS 	  	= createBlock("blue_terracotta_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block BROWN_TERRACOTTA_CHESS 	  	= createBlock("brown_terracotta_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block GREEN_TERRACOTTA_CHESS 	  	= createBlock("green_terracotta_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block RED_TERRACOTTA_CHESS 		  	= createBlock("red_terracotta_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block BLACK_TERRACOTTA_CHESS 	  	= createBlock("black_terracotta_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block WHITE_CONCRETE_CHESS 	  	  	= createBlock("white_concrete_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block ORANGE_CONCRETE_CHESS 	  	= createBlock("orange_concrete_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block MAGENTA_CONCRETE_CHESS 	  	= createBlock("magenta_concrete_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block LIGHT_BLUE_CONCRETE_CHESS   	= createBlock("light_blue_concrete_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block YELLOW_CONCRETE_CHESS 	  	= createBlock("yellow_concrete_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block LIME_CONCRETE_CHESS 	  	  	= createBlock("lime_concrete_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block PINK_CONCRETE_CHESS 	  	  	= createBlock("pink_concrete_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block GRAY_CONCRETE_CHESS 	  	  	= createBlock("gray_concrete_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block LIGHT_GRAY_CONCRETE_CHESS   	= createBlock("light_gray_concrete_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block CYAN_CONCRETE_CHESS 	  	  	= createBlock("cyan_concrete_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block PURPLE_CONCRETE_CHESS 	  	= createBlock("purple_concrete_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block BLUE_CONCRETE_CHESS 	  	  	= createBlock("blue_concrete_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block BROWN_CONCRETE_CHESS 	  	  	= createBlock("brown_concrete_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block GREEN_CONCRETE_CHESS 	  	  	= createBlock("green_concrete_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block RED_CONCRETE_CHESS 			= createBlock("red_concrete_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
+	public static final Block BLACK_CONCRETE_CHESS 	  	  	= createBlock("black_concrete_chess", new ChessBlock(Material.STONE, SoundType.STONE), TableTopCraft.TABLE_TOP_CRAFT_GROUP);
 
 	public static void init() {}
 
--- src/main/java/andrews/table_top_craft/registry/TTCTileEntities.java
+++ src/main/java/andrews/table_top_craft/registry/TTCTileEntities.java
@@ -1,5 +1,6 @@
 package andrews.table_top_craft.registry;
 
+import andrews.table_top_craft.objects.blocks.ChessBlock;
 import andrews.table_top_craft.tile_entities.ChessPieceFigureBlockEntity;
 import andrews.table_top_craft.tile_entities.ChessTileEntity;
 import andrews.table_top_craft.tile_entities.TicTacToeBlockEntity;
@@ -11,11 +12,15 @@ import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TTCTileEntities
 {
-	public static final BlockEntityType<ChessTileEntity> CHESS                              = Registry.register(Registry.BLOCK_ENTITY_TYPE, new ResourceLocation(Reference.MODID, "chess"), FabricBlockEntityTypeBuilder.create(ChessTileEntity::new, TTCBlocks.OAK_CHESS, TTCBlocks.SPRUCE_CHESS, TTCBlocks.BIRCH_CHESS, TTCBlocks.JUNGLE_CHESS, TTCBlocks.ACACIA_CHESS, TTCBlocks.DARK_OAK_CHESS, TTCBlocks.CRIMSON_CHESS, TTCBlocks.WARPED_CHESS).build(null));
+	public static final BlockEntityType<ChessTileEntity> CHESS                              = Registry.register(Registry.BLOCK_ENTITY_TYPE, new ResourceLocation(Reference.MODID, "chess"), FabricBlockEntityTypeBuilder.create(ChessTileEntity::new, TTCTileEntities.getBlocksOfClass(ChessBlock.class)).build(null));
     public static final BlockEntityType<ChessPieceFigureBlockEntity> CHESS_PIECE_FIGURE     = Registry.register(Registry.BLOCK_ENTITY_TYPE, new ResourceLocation(Reference.MODID, "chess_piece_figure"), FabricBlockEntityTypeBuilder.create(ChessPieceFigureBlockEntity::new, TTCBlocks.CHESS_PIECE_FIGURE).build(null));
     public static final BlockEntityType<TicTacToeBlockEntity> TIC_TAC_TOE                   = Registry.register(Registry.BLOCK_ENTITY_TYPE, new ResourceLocation(Reference.MODID, "tic_tac_toe"), FabricBlockEntityTypeBuilder.create(TicTacToeBlockEntity::new, TTCBlocks.TIC_TAC_TOE).build(null));
 
@@ -26,5 +31,15 @@ public class TTCTileEntities
         BlockEntityRendererRegistry.register(CHESS, ChessTileEntityRenderer::new);
         BlockEntityRendererRegistry.register(CHESS_PIECE_FIGURE, ChessPieceFigureTileEntityRenderer::new);
         BlockEntityRendererRegistry.register(TIC_TAC_TOE, TicTacToeBlockEntityRenderer::new);
+    }
+
+    private static Block[] getBlocksOfClass(Class<?> clazz)
+    {
+        List<Block> blocks = new ArrayList<>();
+        Registry.BLOCK.forEach(blockRO -> {
+            if (clazz.isAssignableFrom(blockRO.getClass()))
+                blocks.add(blockRO);
+        });
+        return blocks.toArray(new Block[0]);
     }
 }
